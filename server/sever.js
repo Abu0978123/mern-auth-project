4c08d355cc4529b2ab28281b4c75d4f49b283075
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2023-12-12 23:58:54 +0500

    create test api route

--- server/sever.js
+++ server/sever.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 import { DBconn } from './db/db-conn.js';
 dotenv.config();
 const PORT = process.env.PORT || 30001;
+import useRouter from './routes/user.route.js'
 const app = express();
 DBconn() ;
-
+app.use(express.json());
 
 app.listen((PORT), ()=>{
     console.log(`port is running on ${PORT}`)
 })
+
+app.use('/api/user', useRouter)
