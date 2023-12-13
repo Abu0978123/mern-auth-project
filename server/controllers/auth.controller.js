59cd80fef65889c88a5f80da3a0d04216f512a40
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2023-12-13 16:20:30 +0500

    custom error added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import  User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 
-export const signup = (req, res) => {
+export const signup = (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     newUser.save()    
-    .then(()=> {res.json({message: 'user added successfully'})})
-    .catch((err)=> {res.json(err)})
+    .then(()=> {res.status(201).json({message: 'user added successfully'})})
+    .catch((next)=> {res.status(500).json(next)})
 }
