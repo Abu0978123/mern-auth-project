3a44a1aaaac525a1793bfbe1ba71268cf53fdd51
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2023-12-19 11:13:02 +0500

    protected route created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Sign } from "./pages/Sign"
 import { Signup } from "./pages/Signup"
 import { About } from "./pages/About"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
     <Route path="/sign-in" element={<Sign/>}/>
     <Route path="/sign-up" element={<Signup/>}/>
     <Route path="/about" element={<About/>}/>
+    <Route element={<PrivateRoute />}>
+      <Route path="/profile" element={<Profile/>}/>
+    </Route>
   </Routes>
   </BrowserRouter>
   )
